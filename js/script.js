38dabd8c51572bbd5b82d6df961d67b11fa133be
Anriyan <anriyan2@gmail.com>
2024-06-20 14:44:08 +0700

    js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 // Ini  Javascript
+
+function Konversi() {
+
+}
+
+function Reset() {
+    alert('selamat, mesinnya menyala')
+}
+
+function Reverse() {
+
+}
+
+let celciusinput = document.querySelector('celcius><input')
+let fahrenheitinput = document.querySeletor('fahrenheit><input')
+
+let btn = document.querySelector('.button button')
+
+function roundNumber(number) {
+    return Math.round(number*100)/100
+}
+
+celciusinput.addEventListener('click', function(){
+    let cTemp = parseFloat(celciusInput.value)
+    let fTemp = parseFloat()
+})
